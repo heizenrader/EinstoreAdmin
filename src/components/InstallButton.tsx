c43af7da5bff2c0bdd89b90d42c8b26e8ac9e9cf
David Greco <dave.greco@icloud.com>
2021-10-01 09:44:28 -0600

    removed unused icon

--- src/components/InstallButton.tsx
+++ src/components/InstallButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from './button'
 import IconMobile from '../shapes/mobile'
-import IconDownload from '../shapes/download'
+// import IconDownload from '../shapes/download'
 import { App } from '../connector/Model/App'
 import cn from 'classnames'
 
